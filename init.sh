a41c68e088a9f7e535f1c82a9c154f4720870742
Gregory Karpushin <jimi77.gk@gmail.com>
2022-07-14 15:41:24 +0500

    initial commit

--- init.sh
+++ init.sh
@@ -3,6 +3,9 @@ mkdir -p ~root/.ssh
 cp ~vagrant/.ssh/auth* ~root/.ssh
 yum install -y docker
 mkdir mynginx
-cd /vagrant/ && cp Dockerfile test.conf index.html /home/vagrant/mynginx/
+cp /vagrant/Dockerfile /vagrant/test.conf /vagrant/index.html /home/vagrant/mynginx/
 systemctl enable docker
 systemctl start docker
+cd /home/vagrant/mynginx/
+docker build -t docker-nginx:v1 .
+docker run -d -p 80:8080 docker-nginx:v1
